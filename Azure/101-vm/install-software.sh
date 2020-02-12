99dbda7cf6394563cdbdc878f14e847cb3a159cf
Frédéric Le Coquil <flecoqui@microsoft.com>
2020-02-12 11:41:31 +0100

    Add template

---
+++ Azure/101-vm/install-software.sh
@@ -0,0 +1,385 @@
+#!/bin/bash
+# This bash file install apache
+# Parameter 1 hostname 
+azure_hostname=$1
+rtmp_path=$2
+storage_account_prefix=$3
+storage_sas_token=$4
+#############################################################################
+log()
+{
+	# If you want to enable this logging, uncomment the line below and specify your logging key 
+	#curl -X POST -H "content-type:text/plain" --data-binary "$(date) | ${HOSTNAME} | $1" https://logs-01.loggly.com/inputs/${LOGGING_KEY}/tag/redis-extension,${HOSTNAME}
+	echo "$1"
+	echo "$1" >> /testrtmp/log/install.log
+}
+
+#############################################################################
+check_os() {
+    grep ubuntu /proc/version > /dev/null 2>&1
+    isubuntu=${?}
+    grep centos /proc/version > /dev/null 2>&1
+    iscentos=${?}
+    grep redhat /proc/version > /dev/null 2>&1
+    isredhat=${?}	
+	if [ -f /etc/debian_version ]; then
+    isdebian=0
+	else
+	isdebian=1	
+    fi
+
+	if [ $isubuntu -eq 0 ]; then
+		OS=Ubuntu
+		VER=$(lsb_release -a | grep Release: | sed  's/Release://'| sed -e 's/^[ \t]*//' | cut -d . -f 1)
+	elif [ $iscentos -eq 0 ]; then
+		OS=Centos
+		VER=$(cat /etc/centos-release)
+	elif [ $isredhat -eq 0 ]; then
+		OS=RedHat
+		VER=$(cat /etc/redhat-release)
+	elif [ $isdebian -eq 0 ];then
+		OS=Debian  # XXX or Ubuntu??
+		VER=$(cat /etc/debian_version)
+	else
+		OS=$(uname -s)
+		VER=$(uname -r)
+	fi
+	
+	ARCH=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
+
+	log "OS=$OS version $VER Architecture $ARCH"
+}
+
+
+#############################################################################
+configure_network(){
+# firewall configuration 
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+iptables -A INPUT -p tcp --dport 1935 -j ACCEPT
+}
+#############################################################################
+configure_network_centos(){
+# firewall configuration 
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+iptables -A INPUT -p tcp --dport 1935 -j ACCEPT
+
+
+service firewalld start
+firewall-cmd --permanent --add-port=80/tcp
+firewall-cmd --permanent --add-port=443/tcp
+firewall-cmd --permanent --add-port=1935/tcp
+firewall-cmd --reload
+}
+
+
+
+#############################################################################
+install_git_ubuntu(){
+apt-get -y install git
+}
+install_git_centos(){
+yum -y install git
+}
+
+
+
+
+
+#############################################################################
+build_ngix_rtmp(){
+# install pre-requisites
+apt-get -y install build-essential curl g++
+# Download source code
+cd /git
+#git clone https://github.com/nginx/nginx.git
+wget http://nginx.org/download/nginx-1.16.1.tar.gz
+tar xvfz nginx-1.16.1.tar.gz
+git clone https://github.com/arut/nginx-rtmp-module.git 
+cd nginx-1.16.1
+./configure --add-module=/git/nginx-rtmp-module
+make
+make install
+#/git/nginx-1.16.1/objs/nginx
+#/usr/local/nginx/sbin/
+log "nginx_rtmp built"
+
+}
+
+#############################################################################
+build_ffmpeg(){
+# install pre-requisites
+apt-get -y install build-essential curl g++
+# Download source code
+cd /git
+git clone https://github.com/markus-perl/ffmpeg-build-script.git
+cd ffmpeg-build-script
+export AUTOINSTALL=yes
+./build-ffmpeg --build
+log "ffmpeg built"
+}
+
+build_ffmpeg_rhel(){
+build_ffmpeg
+}
+
+
+#############################################################################
+build_azcopy(){
+cd /git
+wget https://aka.ms/downloadazcopy-v10-linux
+ 
+#Expand Archive
+tar -xvf downloadazcopy-v10-linux
+ 
+#(Optional) Remove existing AzCopy version
+sudo rm /usr/bin/azcopy
+ 
+#Move AzCopy to the destination you want to store it
+sudo cp ./azcopy_linux_amd64_*/azcopy /usr/bin/
+log "azcopy built"
+}
+
+
+
+#############################################################################
+install_ffmpeg(){
+cat <<EOF > /testrtmp/ffmpegloop.sh
+while [ : ]
+do
+folder=$(date  +"%F-%X.%S")
+mkdir /temp/$folder
+/usr/bin/ffmpeg -f flv -i rtmp://10.0.1.4:1935/$1 -c copy -flags +global_header -f segment -segment_time 60 -segment_format_options movflags=+faststart -reset_timestamps 1 -strftime 1 "/temp/$folder/%Y-%m-%d_%H-%M-%S_chunk.mp4" 
+sleep 5
+done
+EOF
+
+chmod +x   /testrtmp/ffmpegloop.sh
+adduser testrtmpuser --disabled-login
+
+cat <<EOF > /etc/systemd/system/ffmpegloop.service
+[Unit]
+Description=ffmpeg Loop Service
+After=network.target
+
+[Service]
+Type=simple
+User=testrtmpuser
+ExecStart=/bin/sh /testrtmp/ffmpegloop.sh
+Restart=on-abort
+
+[Install]
+WantedBy=multi-user.targetEOF
+EOF
+}
+
+#############################################################################
+install_azcopy(){
+cat <<EOF > /testrtmp/azcopyloop.sh
+prefixuri=$1
+sastoken=$2
+while [ : ]
+do
+for mp in /temp/**/*.mp4
+do
+echo "......"
+        echo "$mp"
+#echo "$sastoken"
+#echo "$prefixuri"
+#echo azcopy cp "$mp" "$prefixuri$mp$sastoken"
+lsof | grep $mp
+if [ ! ${?} -eq 0 ];
+then
+        echo copying "$mp"
+        azcopy cp "$mp" "$prefixuri$mp$sastoken"
+        rm -f "$mp"
+else
+        echo in process "$mp"
+fi
+done
+sleep 60
+done
+EOF
+
+chmod +x   /testrtmp/azcopyloop.sh
+adduser testrtmpuser --disabled-login
+
+cat <<EOF > /etc/systemd/system/azcopyloop.service
+[Unit]
+Description=Azcopy Loop Service
+After=network.target
+
+[Service]
+Type=simple
+User=loop
+ExecStart=/bin/sh /testrtmp/azcopyloop.sh
+Restart=on-abort
+
+
+[Install]
+WantedBy=multi-user.target
+EOF
+}
+#############################################################################
+install_nginx_rtmp(){
+/usr/local/nginx/sbin/nginx -s stop
+
+cat <<EOF > /usr/local/nginx/conf/nginx.conf
+#user  nobody;
+worker_processes  1;
+error_log  /temp/error.log debug;
+events {
+    worker_connections  1024;
+}
+http {
+    include       mime.types;
+    default_type  application/octet-stream;
+
+    sendfile        on;
+    keepalive_timeout  65;
+
+    server {
+        listen       80;
+        server_name  localhost;
+
+        # rtmp stat
+        location /stat {
+            rtmp_stat all;
+            rtmp_stat_stylesheet stat.xsl;
+        }
+        location /stat.xsl {
+            # you can move stat.xsl to a different location
+            root /usr/build/nginx-rtmp-module;
+        }
+
+        # rtmp control
+        location /control {
+            rtmp_control all;
+        }
+
+        error_page   500 502 503 504  /50x.html;
+        location = /50x.html {
+            root   html;
+        }
+    }
+}
+
+rtmp {
+    server {
+        listen 1935;
+        ping 30s;
+        notify_method get;
+        buflen 5s;
+
+        application live {
+            live on;
+            interleave on;
+            # exec_push ffmpeg -i rtmp://127.0.0.1:1935/live/stream -c copy -f flv /tmp/test.flv ;
+            # exec_push ffmpeg -f flv -i rtmp://10.0.1.4:1935/live/stream -c copy -flags +global_header -f segment -segment_time 60 -segment_format_options movflags=+faststart -reset_timestamps 1 /temp/testnginx%d.mp4  >> /temp/ffmpeg.log ;
+        }
+    }
+EOF
+
+}
+
+
+
+
+
+
+#############################################################################
+
+environ=`env`
+# Create folders
+mkdir /git
+mkdir /temp
+mkdir /testrtmp
+mkdir /testrtmp/log
+mkdir /testrtmp/config
+
+# Write access in log subfolder
+chmod -R a+rw /testrtmp/log
+log "Environment before installation: $environ"
+
+log "Installation script start : $(date)"
+log "Net Core Installation: $(date)"
+log "#####  azure_hostname: $azure_hostname"
+log "#####  rtmp_path: $artmp_path"
+log "#####  storage_account: $storage_account_prefix"
+log "#####  storage_key: $storage_sas_token"
+log "Installation script start : $(date)"
+check_os
+if [ $iscentos -ne 0 ] && [ $isredhat -ne 0 ] && [ $isubuntu -ne 0 ] && [ $isdebian -ne 0 ];
+then
+    log "unsupported operating system"
+    exit 1 
+else
+	if [ $iscentos -eq 0 ] ; then
+	    log "configure network centos"
+		configure_network_centos
+	    log "install git centos"
+		install_git_centos
+	elif [ $isredhat -eq 0 ] ; then
+	    log "configure network redhat"
+		configure_network_centos
+	    log "install git redhat"
+		install_git_centos
+	elif [ $isubuntu -eq 0 ] ; then
+	    log "configure network ubuntu"
+		configure_network
+	    log "install git ubuntu"
+		install_git_ubuntu
+	elif [ $isdebian -eq 0 ] ; then
+	    log "configure network"
+		configure_network
+	    log "install git debian"
+		install_git_ubuntu
+	fi
+	log "build ffmpeg and nginx_rtmp"
+	if [ $isredhat -eq 0 ] ; then
+	    log "build ffmpeg nginx_rtmp redhat"
+		build_ffmpeg
+		build_nginx_rtmp
+		build_azcopy
+	else
+	    log "build ffmpeg nginx_rtmp "
+		build_ffmpeg
+		build_nginx_rtmp
+		build_azcopy
+	fi
+
+	if [ $iscentos -eq 0 ] ; then
+	    log "install ffmpeg nginx_rtmp azcopy centos"
+		install_ffmpeg $rtmp_path
+		install_nginx_rtmp
+		install_azcopy $storage_account_prefix  $storage_sas_token
+	elif [ $isredhat -eq 0 ] ; then
+	    log "install ffmpeg nginx_rtmp azcopy redhat"
+		install_ffmpeg $rtmp_path
+		install_nginx_rtmp
+		install_azcopy $storage_account_prefix  $storage_sas_token
+	elif [ $isubuntu -eq 0 ] ; then
+	    log "install ffmpeg nginx_rtmp azcopy ubuntu"
+		install_ffmpeg $rtmp_path
+		install_nginx_rtmp
+		install_azcopy $storage_account_prefix  $storage_sas_token
+	elif [ $isdebian -eq 0 ] ; then
+	    log "install ffmpeg nginx_rtmp azcopy debian"
+		install_ffmpeg $rtmp_path
+		install_nginx_rtmp
+		install_azcopy $storage_account_prefix  $storage_sas_token
+	fi
+	log "Start nginx_rtmp service"
+	/usr/local/nginx/sbin/nginx -s stop
+	/usr/local/nginx/sbin/nginx
+	log "Start ffmpeg service"
+	systemctl enable ffmpegloop.service
+	systemctl start ffmpegloop.service 
+	log "Start azcopy service"
+	systemctl enable azcopyloop.service
+	systemctl start azcopyloop.service 
+	log "Installation successful, services nginx_rtmp, ffmpeg and azcopy running"
+fi
+exit 0 
+
