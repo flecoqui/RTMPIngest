070783010240ecae66f8a8d79666fc8528f5ba4a
Frédéric Le Coquil <flecoqui@microsoft.com>
2020-11-01 14:38:02 +0100

    update videojs files to support latest Windows 10 version

--- Azure/101-vm-light-hls/install-software.sh
+++ Azure/101-vm-light-hls/install-software.sh
@@ -499,8 +499,9 @@ cat <<EOF > /usr/local/nginx/html/player.html
 <head>
     <meta charset="UTF-8">
     <title>Live Streaming</title>
-    <link href="//vjs.zencdn.net/5.8/video-js.min.css" rel="stylesheet">
-    <script src="//vjs.zencdn.net/5.8/video.min.js"></script>
+    <link href="//vjs.zencdn.net/7.8.2/video-js.min.css" rel="stylesheet">
+    <script src="//vjs.zencdn.net/7.8.2/video.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/videojs-contrib-eme@3.7.0/dist/videojs-contrib-eme.min.js"></script>
 </head>
 <body>
 <video id="player" class="video-js vjs-default-skin" height="360" width="640" controls preload="none">
