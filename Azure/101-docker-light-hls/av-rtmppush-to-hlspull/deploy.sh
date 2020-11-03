cd9cf4e76d594fd7fd35aadae8c5712c229f3b5e
Frédéric Le Coquil <flecoqui@microsoft.com>
2020-11-03 13:02:39 +0100

    add av-rtmppush-to-hlspull container

--- Azure/101-docker-light-hls/av-rtmppush-to-hlspull/deploy.sh
+++ Azure/101-docker-light-hls/av-rtmppush-to-hlspull/deploy.sh
@@ -1,3 +1,4 @@
 docker build -t flecoqui/av-rtmppush-to-hlspull .
-docker run -p 80:80/tcp  -p 8080:8080/tcp   -p 1935:1935/tcp  flecoqui/av-rtmppush-to-hlspull
+docker run -p 80:80/tcp  -p 8080:8080/tcp   -p 1935:1935/tcp -d  flecoqui/av-rtmppush-to-hlspull
+docker run -p 80:80/tcp  -p 8080:8080/tcp   -p 1935:1935/tcp -d -it flecoqui/av-rtmppush-to-hlspull /bin/bash
 docker push flecoqui/av-rtmppush-to-hlspull
