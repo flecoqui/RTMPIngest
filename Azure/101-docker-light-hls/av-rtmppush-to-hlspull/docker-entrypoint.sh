e2fb50201a0c295d1d525a963bc2cc971b47256d
Frédéric Le Coquil <flecoqui@microsoft.com>
2020-11-03 16:45:53 +0100

    update av-rtmppush-to-hlspull container

--- Azure/101-docker-light-hls/av-rtmppush-to-hlspull/docker-entrypoint.sh
+++ Azure/101-docker-light-hls/av-rtmppush-to-hlspull/docker-entrypoint.sh
@@ -8,31 +8,99 @@ if [ -z "${NGINX_ENTRYPOINT_QUIET_LOGS:-}" ]; then
 else
     exec 3>/dev/null
 fi
+echo '<!DOCTYPE html>\n\
+<html lang="en">\n\
+<head>\n\
+    <meta charset="UTF-8">\n\
+    <title>Live Streaming</title>\n\
+    <link href="//vjs.zencdn.net/7.8.2/video-js.min.css" rel="stylesheet">\n\
+    <script src="//vjs.zencdn.net/7.8.2/video.min.js"></script>\n\
+    <script src="https://cdn.jsdelivr.net/npm/videojs-contrib-eme@3.7.0/dist/videojs-contrib-eme.min.js"></script>\n\
+ </head>\n\
+ <body>\n\
+<video id="player" class="video-js vjs-default-skin" height="360" width="640" controls preload="none">\n\
+    <source src="http://'$HOSTNAME:$PORT_HLS'/hls/stream.m3u8" type="application/x-mpegURL" />\n\
+ </video>\n\
+ <script>\n\
+    var player = videojs("#player");\n\
+ </script>\n\
+ </body>\n\
+ <p>HOSTNAME: '$HOSTNAME'</p>\n\
+ <p>PORT_HLS: '$PORT_HLS'</p>\n\
+ <p>PORT_HTML: '$PORT_HTML'</p>\n\
+ <p>PORT_RTMP: '$PORT_RTMP'</p>\n\
+ </html>\n' > /usr/local/nginx/html/player.html
 
-if [ "$1" = "nginx" -o "$1" = "nginx-debug" ]; then
-    if /usr/bin/find "/docker-entrypoint.d/" -mindepth 1 -maxdepth 1 -type f -print -quit 2>/dev/null | read v; then
-        echo >&3 "$0: /docker-entrypoint.d/ is not empty, will attempt to perform configuration"
+echo "worker_processes  1;\n\
+error_log  /testrtmp/log/nginxerror.log debug;\n\
+events {\n\
+    worker_connections  1024;\n\
+ }\n\
+http {\n\
+    include       mime.types;\n\
+    default_type  application/octet-stream;\n\
+    keepalive_timeout  65;\n\
+    tcp_nopush on;\n\
+    directio 512;\n\
+    server {\n\
+        sendfile        on;\n\
+        listen       "$PORT_HTML";\n\
+        server_name  localhost;\n\
+        location /stat {\n\
+            rtmp_stat all;\n\
+            rtmp_stat_stylesheet stat.xsl;\n\
+        }\n\
+        location /stat.xsl {\n\
+            root /usr/build/nginx-rtmp-module;\n\
+        }\n\
+        location /control {\n\
+            rtmp_control all;\n\
+        }\n\
+        error_page   500 502 503 504  /50x.html;\n\
+        location = /50x.html {\n\
+            root   html;\n\
+        }\n\
+    }\n\
+    server {\n\
+        sendfile        off;\n\
+        listen "$PORT_HLS";\n\
+        location /hls {\n\
+            add_header 'Cache-Control' 'no-cache';\n\
+            add_header 'Access-Control-Allow-Origin' '*' always;\n\
+            add_header 'Access-Control-Expose-Headers' 'Content-Length';\n\
+            if (\$request_method = 'OPTIONS') {\n\
+                add_header 'Access-Control-Allow-Origin' '*';\n\
+                add_header 'Access-Control-Max-Age' 1728000;\n\
+                add_header 'Content-Type' 'text/plain charset=UTF-8';\n\
+                add_header 'Content-Length' 0;\n\
+                return 204;\n\
+            }\n\
+            types {\n\
+                application/dash+xml mpd;\n\
+                application/vnd.apple.mpegurl m3u8;\n\
+                video/mp2t ts;\n\
+            }\n\
+            root /mnt/;\n\
+        }\n\
+    }\n\
+ }\n\
+rtmp {\n\
+    server {\n\
+        listen "$PORT_RTMP";\n\
+        ping 30s;\n\
+        notify_method get;\n\
+        buflen 5s;\n\
+        chunk_size 4000;\n\
+        application live {\n\
+            live on;\n\
+            interleave on;\n\
+            hls on;\n\
+            hls_path /mnt/hls/;\n\
+            hls_fragment 3;\n\
+            hls_playlist_length 60;\n\
+        }\n\
+    }\n\
+}\n" > /usr/local/nginx/conf/nginx.conf
 
-        echo >&3 "$0: Looking for shell scripts in /docker-entrypoint.d/"
-        find "/docker-entrypoint.d/" -follow -type f -print | sort -n | while read -r f; do
-            case "$f" in
-                *.sh)
-                    if [ -x "$f" ]; then
-                        echo >&3 "$0: Launching $f";
-                        "$f"
-                    else
-                        # warn on shell scripts without exec bit
-                        echo >&3 "$0: Ignoring $f, not executable";
-                    fi
-                    ;;
-                *) echo >&3 "$0: Ignoring $f";;
-            esac
-        done
-
-        echo >&3 "$0: Configuration complete; ready for start up"
-    else
-        echo >&3 "$0: No files found in /docker-entrypoint.d/, skipping configuration"
-    fi
-fi
 
 exec "$@"
