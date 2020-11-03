34e3388b06a4feba6b27d1db0dd27cf685176b3e
Frédéric Le Coquil <flecoqui@microsoft.com>
2020-11-03 08:45:11 +0100

    update docker file

--- Azure/101-docker-light-hls/deploy.sh
+++ Azure/101-docker-light-hls/deploy.sh
@@ -1,3 +1,3 @@
 docker build -t flecoqui/my-first-repo .
-docker run flecoqui/my-first-repo
+docker run -p 80:80/tcp  -p 8080:8080/tcp   -p 1935:1935/tcp  flecoqui/my-first-repo
 docker push flecoqui/my-first-repo
