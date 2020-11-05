b38426f78be66d0dc7f78f610d4950635a1690a2
Frédéric Le Coquil <flecoqui@microsoft.com>
2020-11-05 22:26:54 +0100

    update av-rtmppush-to-azurestorage

--- Azure/101-docker-light-hls/av-rtmppull-to-azurestorage/docker-entrypoint.sh
+++ Azure/101-docker-light-hls/av-rtmppull-to-azurestorage/docker-entrypoint.sh
@@ -6,9 +6,9 @@ set -e
 #/bin/bash /testav/ffmpegloop.sh $INPUT_URI
 #/bin/bash /testav/azcliloop.sh  $STORAGE_ACCOUNT $STORAGE_CONTAINER $STORAGE_SASTOKEN
 
-systemctl enable ffmpegloop.service
-systemctl start ffmpegloop.service 
-systemctl enable azcliloop.service
-systemctl start azcliloop.service 
+#systemctl enable ffmpegloop.service
+#systemctl start ffmpegloop.service 
+#systemctl enable azcliloop.service
+#systemctl start azcliloop.service 
 
 exec "$@"
