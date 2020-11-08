4ee95e5113e6b4dccfa4bf55bacbb80f9cd859ff
Frédéric Le Coquil <flecoqui@microsoft.com>
2020-11-08 19:54:32 +0100

    update docker shell

--- Azure/101-docker-light-hls/av-rtmppush-to-rtsppull/docker-entrypoint.sh
+++ Azure/101-docker-light-hls/av-rtmppush-to-rtsppull/docker-entrypoint.sh
@@ -101,8 +101,8 @@ rtmp {
         }
     }
 }" > /usr/local/nginx/conf/nginx.conf
-exec /usr/local/nginx/sbin/nginx -g "daemon off;" &
+exec /usr/local/nginx/sbin/nginx -g "daemon off;" 
 #exec /git/gst-rtsp-server/builddir/examples/test-uri rtmp://127.0.0.1:$PORT_RTMP/live/stream
-exec /git/gst-rtsp-server/builddir/examples/test-uri http://127.0.0.1:$PORT_HLS/hls/stream.m3u8
+#exec /git/gst-rtsp-server/builddir/examples/test-uri http://127.0.0.1:$PORT_HLS/hls/stream.m3u8
 
 exec "$@"
